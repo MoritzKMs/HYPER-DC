9bb0bb6efff8a556783683b5e3f3d8304aaf8447
MoritzKMs <zexyshopp@gmail.com>
2026-09-12 22:46:23 +0300

    Isolate HYPER DC local data storage

--- src/api/DataStore/index.ts
+++ src/api/DataStore/index.ts
@@ -49,7 +49,7 @@ let defaultGetStoreFunc: UseStore | undefined;
 
 function defaultGetStore() {
     if (!defaultGetStoreFunc) {
-        defaultGetStoreFunc = createStore(!IS_REPORTER ? "VencordData" : "VencordDataReporter", "VencordStore");
+        defaultGetStoreFunc = createStore(!IS_REPORTER ? "HyperDCData" : "HyperDCDataReporter", "HyperDCStore");
     }
     return defaultGetStoreFunc;
 }
@@ -277,3 +277,4 @@ export function entries<KeyType extends IDBValidKey, ValueType = any>(
         );
     });
 }
+
